7e67fef4571dd3bc96d0ca7a23ff88669788a0be
Braian <braian.gonzales77@gmail.com>
2023-04-23 17:09:22 +0200

    Clean code

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -11,7 +11,6 @@ import EmailIcon from '@mui/icons-material/Email'
 import LinkedInIcon from '@mui/icons-material/LinkedIn'
 import GitHubIcon from '@mui/icons-material/GitHub'
 
-// TODO: Continuar aquí
 export const Footer = () => {
   const { t } = useTranslation('common')
 
