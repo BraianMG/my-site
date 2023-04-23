92949f648f8c01b514dac38598e63f768c64ef48
Braian <braian.gonzales77@gmail.com>
2023-04-23 19:17:07 +0200

    Fix Footer

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -15,61 +15,50 @@ export const Footer = () => {
   const { t } = useTranslation('common')
 
   return (
-    <Paper component='footer' className={styles.container}>
-      <Box component='div' className={styles.content}>
-        <Box component='div'>
-          <Link href='#'>
-            <Image src={logo} alt='BMG logo' height={40} priority />
-          </Link>
+    <Paper component='footer'>
+      <Box component='div' className={styles.container}>
+        <Box component='div' className={styles.content}>
+          <Box component='div'>
+            <Link href='#'>
+              <Image src={logo} alt='BMG logo' height={40} priority />
+            </Link>
+          </Box>
+          <Box component='div' className={styles.links}>
+            {navItems.map(item => (
+              <Button key={item.href} component='a' href={item.href}>
+                {t(item.name)}
+              </Button>
+            ))}
+          </Box>
+          <Box component='div' className={styles.social}>
+            <Link href='tel:+34 611 69 82 60'>
+              <PhoneIcon />
+            </Link>
+            <Link
+              href={`https://api.whatsapp.com/send?phone=34611698260&text=${t(
+                'whatsAppMessage'
+              )}`}
+              target='_blank'
+            >
+              <WhatsAppIcon />
+            </Link>
+            <Link href='mailto:braian.gonzales77@gmail.com'>
+              <EmailIcon />
+            </Link>
+            <Link
+              href='https://www.linkedin.com/in/braiangonzales/'
+              target='_blank'
+            >
+              <LinkedInIcon />
+            </Link>
+            <Link href='https://github.com/braianmg' target='_blank'>
+              <GitHubIcon />
+            </Link>
+          </Box>
         </Box>
-        <Box component='div' className={styles.links}>
-          {navItems.map(item => (
-            <Button key={item.href} component='a' href={item.href}>
-              {t(item.name)}
-            </Button>
-          ))}
+        <Box component='div' className={styles.copy}>
+          <Typography>{t('copyright')}</Typography>
         </Box>
-        <Box component='div' className={styles.social}>
-          <Link href='tel:+34 611 69 82 60' display='flex' alignItems='center'>
-            <PhoneIcon />
-          </Link>
-          <Link
-            href={`https://api.whatsapp.com/send?phone=34611698260&text=${t(
-              'whatsAppMessage'
-            )}`}
-            target='_blank'
-            display='flex'
-            alignItems='center'
-          >
-            <WhatsAppIcon />
-          </Link>
-          <Link
-            href='mailto:braian.gonzales77@gmail.com'
-            display='flex'
-            alignItems='center'
-          >
-            <EmailIcon />
-          </Link>
-          <Link
-            href='https://www.linkedin.com/in/braiangonzales/'
-            target='_blank'
-            display='flex'
-            alignItems='center'
-          >
-            <LinkedInIcon />
-          </Link>
-          <Link
-            href='https://github.com/braianmg'
-            target='_blank'
-            display='flex'
-            alignItems='center'
-          >
-            <GitHubIcon />
-          </Link>
-        </Box>
-      </Box>
-      <Box component='div' className={styles.copy}>
-        <Typography>{t('copyright')}</Typography>
       </Box>
     </Paper>
   )
